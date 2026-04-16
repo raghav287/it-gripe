91c686a5e3748a8936ae770232bf0a2524d94ab4
raghav287 <raghav@makes360.com>
2026-04-16 13:18:19 +0530

    created the homepage and includes folder for the section that are included in the global pages

---
+++ includes/header.php
@@ -0,0 +1,92 @@
+ <header>
+     <div class="bd-header-transparent-two">
+         <div class="bd-header-top style-three">
+             <div class="bd-header-top-left">
+                 <ul>
+                     <li>
+                         <a href="tel:+918427478157"><span><i
+                                     class="fa-solid fa-phone-volume"></i></span>+918427478157</a>
+                     </li>
+
+                     <li>
+                         <a href="mailto:itfgripe@gmail.com">
+                             <span><i class="fa-sharp fa-light fa-envelope"></i></span>
+                             <span>itfgripe@gmail.com</span>
+                         </a>
+                     </li>
+                 </ul>
+             </div>
+             <div class="bd-header-top-right text-md-end">
+                 <a href="#"><span><i class="fa-sharp fa-regular fa-location-dot"></i></span>123, T12 MRF Sector 117
+                     Mohali 140301</a>
+             </div>
+         </div>
+         <div class="bd-header-area header-style-one" id="header-sticky">
+             <div class="bd-header-inner">
+                 <div class="bd-header-left">
+                     <div class="bd-header-logo">
+                         <a href="index.html"><img src="assets/images/logo/logo.png" alt="logo" /></a>
+                     </div>
+                 </div>
+                 <div class="bd-header-menu">
+                     <nav class="main-menu bd-mobile-menu-active d-none d-xl-block">
+                         <ul>
+                             <li class="home-link">
+                                 <a href="index.html">Home</a>
+                             </li>
+                             <!-- courses menu -->
+                             <li class="menu-item-has-children">
+                                 <a href="javascript:void(0)">Courses</a>
+                                 <ul class="submenu last-children">
+                                     <li class="menu-item-has-children">
+                                         <a href="javascript:void(0)">Learn SQL Using Microsoft SQL Server </a>
+                                     </li>
+                                     <li class="menu-item-has-children">
+                                         <a href="javascript:void(0)">SQL for Interviews: From Basics to Advanced
+                                             Questions</a>
+                                     </li>
+                                     <li class="menu-item-has-children">
+                                         <a href="javascript:void(0)">Soft Skills Development for Career Success</a>
+                                     </li>
+                                 </ul>
+                             </li>
+
+
+                             <!-- About Us -->
+                             <li class="contact-link">
+                                 <a href="#">About Us</a>
+                             </li>
+
+                             <!-- Contact Us -->
+                             <li class="about-link">
+                                 <a href="#">Contact Us</a>
+
+                             </li>
+
+                             <!-- FAQ -->
+                             <li class="contact-link">
+                                 <a href="#">FAQ</a>
+                             </li>
+                         </ul>
+                     </nav>
+                 </div>
+
+                 <!--  Register  -->
+                 <div class="bd-header-right">
+                     <div class="bd-header-sign-btn">
+                         <a class="bd-btn btn-outline-border-primary h-40px" href="#">Register</a>
+                     </div>
+                     <div class="bd-header-hamburger">
+                         <div class="sidebar-toggle">
+                             <a class="bar-icon" href="javascript:void(0)">
+                                 <span></span>
+                                 <span></span>
+                                 <span></span>
+                             </a>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </header>
